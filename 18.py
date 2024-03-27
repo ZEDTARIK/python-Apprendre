c870796b9a31e981b9be16c6503009ab3ed292a3
zouhair.ettarak <ettarak.zouhair@gmail.com>
2024-03-27 23:20:27 +0000

    Pair and Impair Nombre

---
+++ 18.py
@@ -0,0 +1,8 @@
+# This Python code snippet prompts the user to enter a value for `N`. It then converts the input to an
+# integer using `int()`.
+N = int(input("Entre la valeur de N: "))
+
+if N % 2 == 0:
+    print("la valeur est Paire")
+else:
+    print("la valeur est Impaire")
