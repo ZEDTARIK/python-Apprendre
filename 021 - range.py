1b432232b88a2dd002c8650748bc9e6eaf0ac656
zouhair.ettarak <ettarak.zouhair@gmail.com>
2024-03-28 10:33:29 +0000

    Boule For While

---
+++ 021 - range.py
@@ -0,0 +1,13 @@
+N=int(input("Entrez un nombre : "))
+
+# For 
+for i in range(N, N+10) : 
+    print(i, end=" ")
+
+print("------------------")
+
+# While
+j = N+1
+while j <= N+10 :
+    print(j, end=" ")
+    j=j+1
