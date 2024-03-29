9eea73ad2907ef4fbc163f8efb983b9c6932f8ce
zouhair.ettarak <ettarak.zouhair@gmail.com>
2024-03-29 21:57:07 +0000

    Inverse Number

--- 028.py
+++ 028.py
@@ -2,8 +2,10 @@ N= int(input("Entre la valeur de N: "))
 M= N
 
 count = 0
-
+Inverse = 0
 while N != 0 :
+    Inverse = (Inverse * 10) + (N % 10)
     N = N // 10 
     count += 1
 print("Nombre Total de chifre dans le nombre", M, " est", count)
+print("L'inverse de chifre", M, "est :", Inverse)
