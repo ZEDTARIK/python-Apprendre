077fb2ec012cd4015b43d018fc565b0ca1d21f86
zouhair.ettarak <ettarak.zouhair@gmail.com>
2024-04-02 10:15:12 +0000

    Vedio youtube

--- 034.py
+++ 034.py
@@ -1,3 +1,7 @@
+# This Python code prompts the user to enter a value for `L`, and then it generates a pattern of stars
+# and spaces based on the value of `L`. The pattern consists of a square with diagonal lines of stars
+# from the top left to the bottom right and from the top right to the bottom left, as well as stars on
+# the borders of the square.
 L = int(input("Entrez la valeur de L : "))
 
 for i in range(1,L+1) :
