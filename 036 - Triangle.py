3916bdb4e15dea5e0572398a7e845d34eb6e8f05
zouhair.ettarak <ettarak.zouhair@gmail.com>
2024-04-03 11:59:58 +0000

    Triangle egypt step 2

--- 036 - Triangle.py
+++ 036 - Triangle.py
@@ -6,12 +6,30 @@ Ligne = int(input("Entrez le nombre de Ligne : "))
 #   * * * * * * * * * 
 # * * * * * * * * * * * 
 
-for i in range(0, Ligne):
-    for j in range(0, Ligne-i):
+# for i in range(0, Ligne):
+#     for j in range(0, Ligne-i):
+#         print("  ", end="")
+#     for j in range(0, 2*i-1):
+#         print("* ", end="")
+#     print() 
+
+#########################################################
+
+
+#           * 
+#         *   * 
+#       *       * 
+#     *           * 
+#   *               * 
+# * * * * * * * * * * * 
+
+for i in range(1, Ligne+1):
+    for j in range(1, Ligne-i+1):
         print("  ", end="")
-    for j in range(0, 2*i-1):
-        print("* ", end="")
-    print() 
+    for j in range(1, 2*i-1+1):
+        if j == 1 or j == 2*i-1 or i ==Ligne :
+            print("* ", end="")
+        else :
+            print("  ", end="")
+    print()    
 
-
-    
