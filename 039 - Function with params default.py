7113a28014113728a043263a061a82108cb25eb7
zouhair.ettarak <ettarak.zouhair@gmail.com>
2024-04-05 11:53:54 +0000

    function with default params

---
+++ 039 - Function with params default.py
@@ -0,0 +1,9 @@
+def display_employee(name, salary = 3_000):
+    print(f"Name: {name}, Salary: {salary}")
+    
+
+display_employee("Zouhair", 15420)
+display_employee("Ali")
+
+print("Fin du programme")
+    
