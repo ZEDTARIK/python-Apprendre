8527fe50d9923b420d2e1e3dda04c70a3a2ec52c
zouhair.ettarak <ettarak.zouhair@gmail.com>
2024-04-16 10:07:03 +0100

    Try Binary Search

--- binary_searching 2.py
+++ binary_searching 2.py
@@ -1,23 +1,25 @@
-def binary_search(arr, target):
+def binary_Search(arr, target):
     low = 0
-    high = len(arr) -1 
-
+    high = len(arr) - 1
     while low <= high:
         mid = (low + high) // 2
-
         if arr[mid] == target:
             return mid
         elif arr[mid] < target:
             low = mid + 1
         else:
             high = mid - 1
-
     return -1
-    
 
-if __name__ == "__main__":
-    arr = range(1, 100_000_000_000_000_001)
-    print("found in index :", binary_search(arr, 6))
-    print("found in index :", binary_search(arr, 415487))
-    
-    print("found in index :", binary_search(arr, 100_000_000_000_000_000))
+arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+print(f"found index: {binary_Search(arr, 9)}")
+print(f"found index: {binary_Search(arr, 55)}")
+
+names = ['Alice', 'Bob', 'Charlie', 'David', 'Emma', 'Frank', 'Grace', 'Hannah', 'Isaac']
+target_name = 'Frank'
+result_index = binary_Search(names, target_name)
+
+if result_index != -1:
+    print(f"Name '{target_name}' found at index {result_index}.")
+else:
+    print(f"Name '{target_name}' not found in the list.")
