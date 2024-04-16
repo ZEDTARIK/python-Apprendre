7ff80b1c2092a3c9975ec5b32018d3d853ef209e
zouhair.ettarak <ettarak.zouhair@gmail.com>
2024-04-16 20:58:39 +0100

    Skill Python

---
+++ Skills.py
@@ -0,0 +1,22 @@
+# Counting
+l = ['w', 'o', 'o', 'f']
+print(l.count('o')) # 2
+
+s = "agree with you ee"
+print(s.count("e"))  # 4
+
+# Subsetting a list of lists
+
+l = [[7, 6], [8, 9], [10, 11]]
+print(l[1]) # [8, 9]
+
+l = [[7, 6], [8, 9], [10, 11]]
+print(type(l))
+print(l[1][0]) # 8
+print(l[1][1]) # 9
+print(l[-1][1:]) # [11]
+
+
+
+print('I completed ' + str(3) + ' courses!')
+
